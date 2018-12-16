1a483204d5104fd28f6e009e260897d0a010000c
Navin-Baliyase <navinkumar2508@gmail.com>
2018-12-16 15:43:30 +0530

    validations added

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -73,6 +73,6 @@ class AlbumsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def album_params
-      params.require(:album).permit(:name, :owner, photos:[])
+      params.require(:album).permit( :user_id, :name, :owner, photos:[] )
     end
 end
--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,25 @@
 class Album < ApplicationRecord
-  belongs_to :user
-  has_many_attached :photos
-  validates :name, presence: true
-  validates :owner, presence: true
-  
-end
+	belongs_to :user
+	has_many_attached :photos
+	validates :name, presence: true
+	validates :owner, presence: true
+	validate :image_type
+
+	private
+	def image_type
+		if photos.attached? == false
+			errors.add(:photos, "are missing.")
+		end
+		if photos.records.count > 25
+			errors.add(:photos, "limit exceeded.")
+			photos.purge_later
+		end
+		photos.each do |photo|
+			if photo && !photo.content_type.in?(%w(image/jpeg image/png))
+				errors.add(:photo, 'Must be a png a jpeg file')
+					photo.purge_later
+				end
+			end
+		end
+
+	end
